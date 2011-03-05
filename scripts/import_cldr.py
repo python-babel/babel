a119841fd9d4acc15f4a441a78a9881ea4d02efa
Jeroen Ruigrok van der Werven <asmodai@in-nomine.org>
2011-03-05 15:12:39 +0000

    Bump the copyright.

--- scripts/import_cldr.py
+++ scripts/import_cldr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2007 Edgewall Software
+# Copyright (C) 2007-2011 Edgewall Software
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
