b46315d74919a7486705b88b167643c7f222ea8b
Alex Morega <alex@grep.ro>
2013-07-03 16:28:59 +0200

    script to download and import cldr

---
+++ scripts/download_import_cldr.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import shutil
+import zipfile
+import urllib
+import subprocess
+
+
+URL = 'http://unicode.org/Public/cldr/1.9.1/core.zip'
+FILENAME = 'core-1.9.1.zip'
+BLKSIZE = 131072
+
+
+def main():
+    scripts_path = os.path.dirname(os.path.abspath(__file__))
+    repo = os.path.dirname(scripts_path)
+    cldr_path = os.path.join(repo, 'cldr')
+    zip_path = os.path.join(cldr_path, FILENAME)
+
+    if not os.path.isfile(zip_path):
+        with open(zip_path) as f:
+            conn = urllib.open(URL)
+            while True:
+                buf = conn.read(BLKSIZE)
+                if not buf:
+                    break
+                f.write(buf)
+            conn.close()
+
+    common_path = os.path.join(cldr_path, 'common')
+    if os.path.isdir(common_path):
+        shutil.rmtree(common_path)
+
+    z = zipfile.ZipFile(zip_path)
+    z.extractall(cldr_path)
+    z.close()
+
+    subprocess.check_call([
+        sys.executable,
+        os.path.join(scripts_path, 'import_cldr.py'),
+        common_path])
+
+
+if __name__ == '__main__':
+    main()
