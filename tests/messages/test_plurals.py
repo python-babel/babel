37b154952245a83c0645d0db180f16fe7dc1f29b
Alex Morega <alex@grep.ro>
2013-07-06 14:20:28 +0200

    copy babel.messages.plural doctests as unit tests

--- tests/messages/test_plurals.py
+++ tests/messages/test_plurals.py
@@ -16,9 +16,19 @@ import unittest
 
 from babel.messages import plurals
 
+
+def test_get_plural():
+    assert plurals.get_plural(locale='en') == (2, '(n != 1)')
+    assert plurals.get_plural(locale='ga') == (3, '(n==1 ? 0 : n==2 ? 1 : 2)')
+
+    tup = plurals.get_plural("ja")
+    assert tup.num_plurals == 1
+    assert tup.plural_expr == '0'
+    assert tup.plural_forms == 'npurals=1; plural=0'
+    assert str(tup) == 'npurals=1; plural=0'
+
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(doctest.DocTestSuite(plurals))
     return suite
 
 if __name__ == '__main__':
