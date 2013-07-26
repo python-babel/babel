7f3a787df6cffd3aaa15d069a59a2d77e525b7f4
Armin Ronacher <armin.ronacher@active-4.com>
2013-07-26 18:06:09 +0200

    Removed old copyright header from setup file

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright (C) 2007-2011 Edgewall Software
-# All rights reserved.
-#
-# This software is licensed as described in the file COPYING, which
-# you should have received as part of this distribution. The terms
-# are also available at http://babel.edgewall.org/wiki/License.
-#
-# This software consists of voluntary contributions made by many
-# individuals. For the exact contribution history, see the revision
-# history and logs, available at http://babel.edgewall.org/log/.
 
 import os
 import sys
