f1e1cb213860aee44f2e20f93c1bb6a31ed01892
Armin Ronacher <armin.ronacher@active-4.com>
2013-07-26 18:15:02 +0200

    Ready for 2.0-dev

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class import_cldr(Command):
 
 setup(
     name='Babel',
-    version='1.0',
+    version='2.0-dev',
     description='Internationalization utilities',
     long_description=\
 """A collection of tools for internationalizing Python applications.""",
