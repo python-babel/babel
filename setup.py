21cedd6b250d90a5e15d87e7d2db98c810a95839
Armin Ronacher <armin.ronacher@active-4.com>
2013-07-27 11:37:59 +0200

    Bump version number to 1.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class import_cldr(Command):
 
 setup(
     name='Babel',
-    version='1.1-dev',
+    version='1.1',
     description='Internationalization utilities',
     long_description=\
 """A collection of tools for internationalizing Python applications.""",
