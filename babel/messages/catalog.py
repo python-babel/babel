88c564228d30a6cda6368cf2077341fea0c6c6d2
Alex Morega <alex@grep.ro>
2014-01-06 21:55:27 +0200

    parse datetime values with no timezone info
    fixes #56

--- babel/messages/catalog.py
+++ babel/messages/catalog.py
@@ -41,32 +41,35 @@ PYTHON_FORMAT = re.compile(r'''(?x)
 
 
 def _parse_datetime_header(value):
-    match = re.match(r'^(?P<datetime>.*)(?P<tzoffset>[+-]\d{4})$', value)
+    match = re.match(r'^(?P<datetime>.*?)(?P<tzoffset>[+-]\d{4})?$', value)
 
     tt = time.strptime(match.group('datetime'), '%Y-%m-%d %H:%M')
     ts = time.mktime(tt)
+    dt = datetime.fromtimestamp(ts)
 
     # Separate the offset into a sign component, hours, and # minutes
     tzoffset = match.group('tzoffset')
-    plus_minus_s, rest = tzoffset[0], tzoffset[1:]
-    hours_offset_s, mins_offset_s = rest[:2], rest[2:]
+    if tzoffset is not None:
+        plus_minus_s, rest = tzoffset[0], tzoffset[1:]
+        hours_offset_s, mins_offset_s = rest[:2], rest[2:]
 
-    # Make them all integers
-    plus_minus = int(plus_minus_s + '1')
-    hours_offset = int(hours_offset_s)
-    mins_offset = int(mins_offset_s)
+        # Make them all integers
+        plus_minus = int(plus_minus_s + '1')
+        hours_offset = int(hours_offset_s)
+        mins_offset = int(mins_offset_s)
 
-    # Calculate net offset
-    net_mins_offset = hours_offset * 60
-    net_mins_offset += mins_offset
-    net_mins_offset *= plus_minus
+        # Calculate net offset
+        net_mins_offset = hours_offset * 60
+        net_mins_offset += mins_offset
+        net_mins_offset *= plus_minus
 
-    # Create an offset object
-    tzoffset = FixedOffsetTimezone(net_mins_offset)
+        # Create an offset object
+        tzoffset = FixedOffsetTimezone(net_mins_offset)
 
-    # Store the offset in a datetime object
-    dt = datetime.fromtimestamp(ts)
-    return dt.replace(tzinfo=tzoffset)
+        # Store the offset in a datetime object
+        dt = dt.replace(tzinfo=tzoffset)
+
+    return dt
 
 
 class Message(object):
