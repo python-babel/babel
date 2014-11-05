96aaae1afde89d2f32ea2c6eb47eb3043f63181b
Isaac Jurado <diptongo@gmail.com>
2014-11-05 17:45:18 +0100

    fix the colombian spanish formatting
    According to the colombian locales, the currency symbol is placed at the
    beginning, like in english locales.  This needs to be changed because "es_CO"
    now inherits the definitions from "es_419", thus conforming to the standard.

--- babel/numbers.py
+++ babel/numbers.py
@@ -257,7 +257,7 @@ def format_currency(number, currency, format=None, locale=LC_NUMERIC):
     >>> format_currency(1099.98, 'USD', locale='en_US')
     u'$1,099.98'
     >>> format_currency(1099.98, 'USD', locale='es_CO')
-    u'1.099,98\\xa0US$'
+    u'US$1.099,98'
     >>> format_currency(1099.98, 'EUR', locale='de_DE')
     u'1.099,98\\xa0\\u20ac'
 
