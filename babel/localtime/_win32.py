a7c09615394a9dc8deeef17e30d2c7800e67e082
Erik Cederstrand <erik@cederstrand.dk>
2017-01-10 20:29:43 +0100

    Python 2.6 doesn't like dict comprehensions

--- babel/localtime/_win32.py
+++ babel/localtime/_win32.py
@@ -15,7 +15,7 @@ import pytz
 # fail.  We want to catch it down in that case then and just assume
 # the mapping was empty.
 try:
-    tz_names = {v: k for k, v in get_global('windows_zone_mapping').items()}
+    tz_names = dict([(v, k) for k, v in get_global('windows_zone_mapping').items()])
 except RuntimeError:
     tz_names = {}
 
