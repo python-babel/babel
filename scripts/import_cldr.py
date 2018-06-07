4b5097f6064dda281ce7a1c5ef74e047cfdc558c
Luke Plant <L.Plant.98@cantab.net>
2018-06-07 14:37:44 +0300

    Skip non-latn number formats for currency unit patterns

--- scripts/import_cldr.py
+++ scripts/import_cldr.py
@@ -906,10 +906,13 @@ def parse_currency_formats(data, tree):
 
 def parse_currency_unit_patterns(data, tree):
     currency_unit_patterns = data.setdefault('currency_unit_patterns', {})
-    for unit_pattern_elem in tree.findall('.//currencyFormats/unitPattern'):
-        count = unit_pattern_elem.attrib['count']
-        pattern = text_type(unit_pattern_elem.text)
-        currency_unit_patterns[count] = pattern
+    for currency_formats_elem in tree.findall('.//currencyFormats'):
+        if _should_skip_number_elem(data, currency_formats_elem):  # TODO: Support other number systems
+            continue
+        for unit_pattern_elem in currency_formats_elem.findall('./unitPattern'):
+            count = unit_pattern_elem.attrib['count']
+            pattern = text_type(unit_pattern_elem.text)
+            currency_unit_patterns[count] = pattern
 
 
 def parse_day_period_rules(tree):
