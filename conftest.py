512092be0028d3bd3a9c1fc2b379a6bf1110bf8a
Aarni Koskela <akx@iki.fi>
2020-11-10 11:50:43 +0200

    Support Py.test 6+

--- conftest.py
+++ conftest.py
@@ -8,4 +8,7 @@ babel_path = local(__file__).dirpath().join('babel')
 def pytest_collect_file(path, parent):
     if babel_path.common(path) == babel_path:
         if path.ext == ".py":
+            # TODO: remove check when dropping support for old Pytest
+            if hasattr(DoctestModule, "from_parent"):
+                return DoctestModule.from_parent(parent, fspath=path)
             return DoctestModule(path, parent)
