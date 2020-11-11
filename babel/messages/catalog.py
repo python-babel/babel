bd836a4ac51958c083ff13ea02f8c18b7c0f4e2d
Aarni Koskela <akx@iki.fi>
2020-11-11 11:27:58 +0200

    Bump copyright year from 2019 to 2020

--- babel/messages/catalog.py
+++ babel/messages/catalog.py
@@ -5,7 +5,7 @@
 
     Data structures for message catalogs.
 
-    :copyright: (c) 2013-2019 by the Babel Team.
+    :copyright: (c) 2013-2020 by the Babel Team.
     :license: BSD, see LICENSE for more details.
 """
 
