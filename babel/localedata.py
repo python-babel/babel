70aa6763ab6b73e9dfb3961250875be2d4149536
Jonah Lawrence <jonah@freshidea.com>
2023-01-25 17:08:32 -0700

    try typing.MutableMapping for 3.7-3.8 support

--- babel/localedata.py
+++ babel/localedata.py
@@ -18,11 +18,10 @@ import pickle
 import re
 import sys
 import threading
-from collections import abc
 from collections.abc import Iterator, Mapping, MutableMapping
 from functools import lru_cache
 from itertools import chain
-from typing import Any, TypeVar
+from typing import Any, TypeVar, MutableMapping
 
 _Key = TypeVar('_Key',)
 _Value = TypeVar('_Value')
