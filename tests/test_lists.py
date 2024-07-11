64e80281058a0dabcd8f2c435f4c109638c8b07d
Aarni Koskela <akx@iki.fi>
2024-07-11 13:43:22 +0300

    Use conventional parametrize in list tests

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -3,16 +3,16 @@ import pytest
 from babel import lists
 
 
-def test_format_list():
-    for list, locale, expected in [
-        ([], 'en', ''),
-        (['string'], 'en', 'string'),
-        (['string1', 'string2'], 'en', 'string1 and string2'),
-        (['string1', 'string2', 'string3'], 'en', 'string1, string2, and string3'),
-        (['string1', 'string2', 'string3'], 'zh', 'string1、string2和string3'),
-        (['string1', 'string2', 'string3', 'string4'], 'ne', 'string1,string2, string3 र string4'),
-    ]:
-        assert lists.format_list(list, locale=locale) == expected
+@pytest.mark.parametrize(('list', 'locale', 'expected'), [
+    ([], 'en', ''),
+    (['string'], 'en', 'string'),
+    (['string1', 'string2'], 'en', 'string1 and string2'),
+    (['string1', 'string2', 'string3'], 'en', 'string1, string2, and string3'),
+    (['string1', 'string2', 'string3'], 'zh', 'string1、string2和string3'),
+    (['string1', 'string2', 'string3', 'string4'], 'ne', 'string1,string2, string3 र string4'),
+])
+def test_format_list(list, locale, expected):
+    assert lists.format_list(list, locale=locale) == expected
 
 
 def test_format_list_error():
