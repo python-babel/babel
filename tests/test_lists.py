1a45abace1d4535a0c5edeb9bf822bc1c1c57589
Aarni Koskela <akx@iki.fi>
2024-07-11 14:54:16 +0300

    Support list format fallbacks

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -1,6 +1,6 @@
 import pytest
 
-from babel import lists
+from babel import lists, units
 
 
 @pytest.mark.parametrize(('list', 'locale', 'expected'), [
@@ -18,3 +18,15 @@ def test_format_list(list, locale, expected):
 def test_format_list_error():
     with pytest.raises(ValueError):
         lists.format_list(['a', 'b', 'c'], style='orange', locale='en')
+
+
+def test_issue_1098():
+    one_foot = units.format_unit(1, "length-foot", length="short", locale="zh_CN")
+    five_inches = units.format_unit(5, "length-inch", length="short", locale="zh_CN")
+    # zh-CN does not specify the "unit" style, so we fall back to "unit-short" style.
+    assert (
+        lists.format_list([one_foot, five_inches], style="unit", locale="zh_CN") ==
+        lists.format_list([one_foot, five_inches], style="unit-short", locale="zh_CN") ==
+        # Translation verified using Google Translate. It would add more spacing, but the glyphs are correct.
+        "1英尺5英寸"
+    )
