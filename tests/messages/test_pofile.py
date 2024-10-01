71dfd0ef23d855871a5d7443681dfd9a0cd9c802
gabe-sherman <gabesherman6@gmail.com>
2024-09-30 21:05:46 -0600

    additional test cases added for issue 1134

--- tests/messages/test_pofile.py
+++ tests/messages/test_pofile.py
@@ -1016,8 +1016,37 @@ msgstr ""
     assert pofile.read_po(buf).locale is None
 
 def test_issue_1134():
+    # creating multiple copies of IO objects so function calls do not effect later calls.
     buf = StringIO('''
 msgid "foo"
 "''')
-    with pytest.raises(pofile.PoFileError):
+
+    # Catalog is created with warning, no abort
+    output = pofile.read_po(buf)
+    assert isinstance(output, Catalog)
+
+    buf = StringIO('''
+msgid "foo"
+"''')
+
+    # Catalog not created, aborted with PoFileError
+    with pytest.raises(pofile.PoFileError) as excinfo:
         pofile.read_po(buf, abort_invalid=True)
+    assert(str(excinfo.value)) == "missing msgstr for msgid 'foo' on 1"
+
+    buf = StringIO('''
+msgid "foo"
+msgid_plural "foos"
+"''')
+
+    # Catalog not created, aborted with PoFileError
+    with pytest.raises(pofile.PoFileError) as e:
+        pofile.read_po(buf, abort_invalid=True)
+    assert(str(e.value)) == "missing msgstr for msgid 'foo' on 1"
+
+
+
+
+
+    
+
