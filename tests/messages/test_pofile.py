df1072f671789ddf9f5e6a43984ea0945b7bdca9
Rodrigo Tobar <rtobar@icrar.org>
2024-11-17 11:08:55 +0800

    Remove duplicate test assertion
    The exact same check is performed a few lines above.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- tests/messages/test_pofile.py
+++ tests/messages/test_pofile.py
@@ -544,11 +544,6 @@ msgstr "bar"
         output = pofile.read_po(buf, locale='fr', abort_invalid=False)
         assert isinstance(output, Catalog)
 
-        # Catalog not created, aborted with PoFileError
-        buf = StringIO(invalid_po_2)
-        with pytest.raises(pofile.PoFileError):
-            pofile.read_po(buf, locale='fr', abort_invalid=True)
-
     def test_invalid_pofile_with_abort_flag(self):
         parser = pofile.PoFileParser(None, abort_invalid=True)
         lineno = 10
