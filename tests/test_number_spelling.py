0cc8c139374844988ef6c4fd77a2775f22c536c8
Szabolcs <szabolcs.blaga@gmail.com>
2025-09-28 20:24:16 +0200

    for python versions below 3.11 skip toml based rbnf tests

--- tests/test_number_spelling.py
+++ tests/test_number_spelling.py
@@ -1,7 +1,6 @@
-import unittest
+import sys
 
 import pytest
-import tomllib
 
 from babel import numbers, rbnf
 from babel.localedata import locale_identifiers
@@ -9,60 +8,58 @@ from babel.localedata import locale_identifiers
 soft_hyphen = '\xad'
 
 
-class TestRuleEngine(unittest.TestCase):
+def test_basic():
+    x = rbnf.RuleBasedNumberFormat.negotiate('hu_HU')
+    assert str(x._locale) == 'hu'
+    assert 'spellout-numbering' in x.available_rulesets
+
+
+def test_negotiation():
+    for lid in locale_identifiers():
+        try:
+            loc = rbnf.RuleBasedNumberFormat.negotiate(lid)._locale
+        except rbnf.RulesetNotFound:
+            # generate warning if necessary
+            continue
+        # test groups
+        for k in loc._data['rbnf_rules']:
+            assert k in rbnf.RuleBasedNumberFormat.group_types
+
+
+def test_tokenization():
+    x = list(rbnf.tokenize("text[opt];"))
+    res = [
+        rbnf.TokenInfo(type=1, reference='text', optional=False),
+        rbnf.TokenInfo(type=1, reference='opt', optional=True),
+    ]
+    assert x == res
+
+
+def test_xml_parsing():
     """
-    Test everything related to the rules engine
+    all the rules should be able to go through the parser and tokenizer
+    made up some rules and run the tokenizer on them
+
+    TODO
+    read data from all the locales that have rbnf_rules defined
+    all the raw rules should be in a specific structure based
+    on the XML specification
     """
+    assert True
 
-    def test_basic(self):
-        x = rbnf.RuleBasedNumberFormat.negotiate('hu_HU')
-        assert str(x._locale) == 'hu'
-        assert 'spellout-numbering' in x.available_rulesets
 
-    def test_negotiation(self):
-        for lid in locale_identifiers():
-            try:
-                loc = rbnf.RuleBasedNumberFormat.negotiate(lid)._locale
-            except rbnf.RulesetNotFound:
-                # generate warning if necessary
-                continue
-            # test groups
-            for k in loc._data['rbnf_rules']:
-                assert k in rbnf.RuleBasedNumberFormat.group_types
-
-    def test_tokenization(self):
-
-        x = list(rbnf.tokenize("text[opt];"))
-        res = [
-            rbnf.TokenInfo(type=1, reference='text', optional=False),
-            rbnf.TokenInfo(type=1, reference='opt', optional=True),
-        ]
-        assert x == res
-
-    def test_xml_parsing(self):
-        """
-        all the rules should be able to go through the parser and tokenizer
-        made up some rules and run the tokenizer on them
-
-        TODO
-        read data from all the locales that have rbnf_rules defined
-        all the raw rules should be in a specific structure based
-        on the XML specification
-        """
-        assert True
-
-    def test_compute_divisor(self):
-        for rule, divisor in (
-            (1001, 1000),
-            (1_000_001, 1_000_000),
-            (1_000_000_001, 1_000_000_000),
-            (1_000_000_000_000_000_001, 1_000_000_000_000_000_000),
-            (1001, 1000),
-            (1_000_000, 1_000_000),
-            (1_000_000_000, 1_000_000_000),
-            (1_000_000_000_000_000_000, 1_000_000_000_000_000_000),
-        ):
-            assert rbnf.compute_divisor(rule, 10) == divisor
+def test_compute_divisor():
+    for rule, divisor in (
+        (1001, 1000),
+        (1_000_001, 1_000_000),
+        (1_000_000_001, 1_000_000_000),
+        (1_000_000_000_000_000_001, 1_000_000_000_000_000_000),
+        (1001, 1000),
+        (1_000_000, 1_000_000),
+        (1_000_000_000, 1_000_000_000),
+        (1_000_000_000_000_000_000, 1_000_000_000_000_000_000),
+    ):
+        assert rbnf.compute_divisor(rule, 10) == divisor
 
 
 @pytest.mark.all_rbnf_locales
@@ -77,7 +74,9 @@ def test_spelling_smoke(locale, ruleset):
 
 
 @pytest.mark.all_rbnf_locales
+@pytest.mark.skipif(sys.version_info < (3, 11), reason="requires python3.11 or higher for tomllib")
 def test_spelling_smoke_toml(locale):
+    import tomllib
 
     speller = rbnf.RuleBasedNumberFormat.negotiate(locale)
 
@@ -94,30 +93,3 @@ def test_spelling_smoke_toml(locale):
                     print(f"RBNFError for {locale} in {ruleset} spelling {number}: {e}")
                     continue
                 assert result == expected, f"{locale} {ruleset} {number}: expected {expected}, got {result}"
-
-
-# def test_hu_HU_error():
-#     with pytest.raises(exceptions.TooBigToSpell) as excinfo:
-#         _spell(10**66, ordinal=True)
-
-#     with pytest.raises(exceptions.PrecisionError) as excinfo:
-#         _spell(.4326752, locale='hu_HU', precision=7)
-
-#     with pytest.raises(exceptions.PrecisionError) as excinfo:
-#         _spell(.4326752)
-
-#     with pytest.raises(exceptions.NoFractionOrdinalsAllowed) as excinfo:
-#         _spell('1999.23862', ordinal=True)
-
-# def test_en_GB_error():
-#     with pytest.raises(exceptions.TooBigToSpell) as excinfo:
-#         _spell(10**24, ordinal=True, locale='en_GB')
-
-#     with pytest.raises(exceptions.PrecisionError) as excinfo:
-#         _spell(.4326752, locale='en_GB', precision=4)
-
-#     with pytest.raises(exceptions.PrecisionError) as excinfo:
-#         _spell(.4326752, locale='en_GB')
-
-#     with pytest.raises(exceptions.NoFractionOrdinalsAllowed) as excinfo:
-#         _spell('1999.23', ordinal=True, locale='en_GB')
